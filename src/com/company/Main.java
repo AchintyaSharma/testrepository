e621ffdb3eed080a37c6bae7322bbcd4cc5ba2fb
achintya <achintya.sharma17@gmail.com>
2017-12-27 00:54:00 +0530

    changes...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        long time = System.currentTimeMillis();
+        List<Integer> ints = new ArrayList<>();
+        ints.add(1);
+        ints.add(2);
+        ints.add(3);
+        ints.add(4);
+        Integer s = 0;
+        for (Integer n : ints) { s += n; }
+        System.out.println(System.currentTimeMillis() - time);
     }
 }
